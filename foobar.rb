1ce33fd65fe78e7fd3080c0e07cae99f273a1d1c
Xin Yi Chen <xinyichen516@gmail.com>
2017-09-19 17:51:00 -0700

    completed hw1

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,22 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+      i = 0
+      a.sort
+      total = 0;
+      until i >= a.length
+      	a[i] = a[i].to_i
+	  	if a[i] % 2 != 0 or a[i] >= 8
+	  		a[i] = 0
+	  	else
+	  		a[i] = a[i] + 2
+	  	end
+	  	if i > 0 and a[i] == a[i - 1]
+	  		a[i] = 0
+	  	end
+	  	total = total + a[i]
+	  	i = i + 1
+      end
+      return total
   end
 end
-
-
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,25 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  return (a + b) ** 2
 
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
+  a = a.sort
+  i = 0
+  until i >= a.length
+  	a[i] = a[i] + 1
+  	i = i + 1
+  end
+
+  return a
 
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
-
+  first_name.concat(" ")
+  first_name.concat(last_name)
+  return first_name
 end
 
 def blockin_time(a)
